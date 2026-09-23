a3cf72b9d84144d845ad681a9482a48bd5473d62
sooyoung0927 <sooyoung040927@gmail.com>
2026-09-23 16:24:37 +0900

    기본 폴더 구조 / 코드 레빗

---
+++ Tukk_Tak/src/main/java/com/ohgiraffer/tukk_tak/tukk_tak/TukkTakApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffer.tukk_tak.tukk_tak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukkTakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TukkTakApplication.class, args);
+    }
+
+}
---
+++ Tukk_Tak/src/test/java/com/ohgiraffer/tukk_tak/tukk_tak/TukkTakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffer.tukk_tak.tukk_tak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukkTakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
